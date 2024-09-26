161c2f5e0a5e11e745793316ed7ee06e825f70a3
FiorellaGallo <fioreg96@gmail.com>
2024-09-26 18:53:05 -0300

    Modificaciones

--- ejercicio-4/main.c
+++ ejercicio-4/main.c
@@ -6,15 +6,17 @@
 
 void validaciones (int *num, int *base){
     int resultado;
-    char c;
+    
 
-    do {
-        if(scanf("%c",&c)){
-        printf("Entrada no válida.Introduce un número decimal:\n");
-        while ((c = getchar()) != '\n' && c != EOF);
-
+    while (1) {
+        printf("Introduce un número decimal: ");
+        if (scanf("%d", num) == 1) {
+            break;  
+        } else {
+            printf("Entrada no válida. Introduce un número decimal.\n");
+            while (fgetc(stdin) != '\n'); 
         }
-    }while(scanf("%d", num) == 0);
+    }
 
     do {
         printf("Introduce un número para la base (entre 2 y 16): ");
@@ -33,7 +35,6 @@ int main()
 {
     Pila P1 = p_crear();
     int num ,base;
-    printf("Introduce un número decimal: ");
     validaciones(&num,&base);
     conversion(num,P1,base);
 }
